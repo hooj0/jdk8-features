399974b7ea702ae4be2cfe1f79b6531c8f967f8e
hoojo <hoojo@qq.com>
2017-09-28 17:39:32 +0800

    init

---
+++ src/com/cnblogs/hoojo/longadder/LongAdderExample.java
@@ -0,0 +1,16 @@
+package com.cnblogs.hoojo.longadder;
+
+/**
+ * <b>function:</b> LongAdder 多线程安全实例
+ * @author hoojo
+ * @createDate 2017年9月1日 下午3:06:38
+ * @file LongAdderExample.java
+ * @package com.cnblogs.hoojo.longadder
+ * @project jdk8-features
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class LongAdderExample {
+
+}
