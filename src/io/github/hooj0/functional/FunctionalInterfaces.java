d90a481a7009a876e61b7d6ca18874e04ed9daf7
hoojo <hoojo@qq.com>
2019-06-08 14:50:55 +0800

    :sparkles: :bento: :tada:  Init add 函数式接口编程

---
+++ src/io/github/hooj0/functional/FunctionalInterfaces.java
@@ -0,0 +1,20 @@
+package io.github.hooj0.functional;
+
+/**
+ * 函数式接口编程
+ * @author hoojo
+ * @createDate 2019年6月8日 上午10:52:52
+ * @file FunctionalInterfaces.java
+ * @package io.github.hooj0.functional
+ * @project jdk8-features
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class FunctionalInterfaces {
+
+	public static void main(String[] args) {
+
+	}
+
+}
