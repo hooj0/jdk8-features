7fd297be2525375bde6c48effd765da159769358
hoojo <hoojo@qq.com>
2019-06-10 17:19:23 +0800

    :sparkles: :bento: :tada:  Init add lambda method expression example

---
+++ src/io/github/hooj0/lambda/LambdaMethodExample.java
@@ -0,0 +1,16 @@
+package io.github.hooj0.lambda;
+
+/**
+ * lambda method expression example
+ * @author hoojo
+ * @createDate 2019年6月9日 下午5:17:49
+ * @file LambdaMethodExample.java
+ * @package io.github.hooj0.lambda
+ * @project jdk8-features
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class LambdaMethodExample {
+
+}
