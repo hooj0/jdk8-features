744b7d506e11e568d6d5249a3266bae25d718b93
hoojo <hoojo@qq.com>
2019-06-12 18:00:14 +0800

    :sparkles: :bento: :tada:  Init add lambda constructor expressions example

---
+++ src/io/github/hooj0/lambda/LambdaConstructorsExample.java
@@ -0,0 +1,40 @@
+package io.github.hooj0.lambda;
+
+/**
+ * lambda constructor expressions example
+ * 
+ * @author hoojo
+ * @createDate 2019年6月12日 下午5:50:32
+ * @file LambdaConstructorsExample.java
+ * @package io.github.hooj0.lambda
+ * @project jdk8-features
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class LambdaConstructorsExample {
+
+	static class User {
+		String name;
+		int age;
+		
+		User(String name, int age) {
+			this.name = name;
+			this.age = age;
+		}
+	}
+	
+	interface UserFactory<U extends User> {
+	    U create(String name, int age);
+	}
+	
+	public static void main(String[] args) {
+
+		// 利用Lambda表达式 new 直接实现UserFactory接口的create方法
+		UserFactory<User> factory = User::new;
+		User user = factory.create("joe", 29);
+		
+		System.out.println(user.name); 	// joe
+		System.out.println(user.age);	// 29
+	}
+}
