1dd1a9bf0a43b1b4e2d7e063d4445289e04c49f0
hoojo <hoojo@qq.com>
2019-06-17 11:35:43 +0800

    :sparkles: :bento: :tada:  Init add Built-in Functional Interfaces Functions Example

---
+++ src/io/github/hooj0/built_in_functional/FunctionsExample.java
@@ -0,0 +1,20 @@
+package io.github.hooj0.built_in_functional;
+
+/**
+ * Built-in Functional Interfaces Functions Example
+ * @author hoojo
+ * @createDate 2019年6月17日 上午11:33:16
+ * @file FunctionsExample.java
+ * @package io.github.hooj0.built_in_functional
+ * @project jdk8-features
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class FunctionsExample {
+
+	public static void main(String[] args) {
+
+	}
+
+}
