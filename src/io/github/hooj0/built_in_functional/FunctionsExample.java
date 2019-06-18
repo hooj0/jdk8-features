2eac856f459330e833e8a475cae32923b7f8bdbe
hoojo <hoojo@qq.com>
2019-06-18 16:56:34 +0800

    :sparkles: :bento: :recycle:  Changed Built-in Functional Interfaces Functions Example

--- src/io/github/hooj0/built_in_functional/FunctionsExample.java
+++ src/io/github/hooj0/built_in_functional/FunctionsExample.java
@@ -1,7 +1,10 @@
 package io.github.hooj0.built_in_functional;
 
+import java.util.function.Function;
+
 /**
  * Built-in Functional Interfaces Functions Example
+ * 函数接受一个参数并产生一个结果。默认方法可用于将多个函数链接在一起（compose和then）。
  * @author hoojo
  * @createDate 2019年6月17日 上午11:33:16
  * @file FunctionsExample.java
@@ -15,6 +18,15 @@ public class FunctionsExample {
 
 	public static void main(String[] args) {
 
+		Function<String, Integer> convter = Integer::valueOf;
+		System.out.println(convter.apply("123") + 2); // 125
+		
+		Function<String, String> rollback = convter.andThen(String::valueOf);
+		System.out.println(rollback.apply("123")); // 123
+		
+		// compose 在apply方法之前完成一些业务处理
+		System.out.println(convter.compose((v) -> {
+			return v.toString() + "2";
+		}).apply(223));
 	}
-
 }
