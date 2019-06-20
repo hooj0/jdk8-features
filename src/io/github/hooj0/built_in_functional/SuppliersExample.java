5caf8ece9620b84c7fc52be8217752712b251ab9
hoojo <hoojo@qq.com>
2019-06-20 17:17:57 +0800

    :sparkles: :bento: :recycle:  Changed Built-in Functional Interfaces Suppliers Example

--- src/io/github/hooj0/built_in_functional/SuppliersExample.java
+++ src/io/github/hooj0/built_in_functional/SuppliersExample.java
@@ -2,6 +2,8 @@ package io.github.hooj0.built_in_functional;
 
 import java.util.function.Supplier;
 
+import io.github.hooj0.lambda.LambdaConstructorsExample.User;
+
 /**
  * Built-in Functional Interfaces Suppliers Example
  * 供应商生成给定泛型类型的结果。与函数不同，供应商不接受参数。
@@ -16,17 +18,13 @@ import java.util.function.Supplier;
  */
 public class SuppliersExample {
 	
-	static class Dog {
-		int age;
-	}
-
 	public static void main(String[] args) {
 
 		// 生成指定泛型类型的对象，但不接受参数
 		Supplier<Double> supplier = Math::random;
 		System.out.println(supplier.get());
 		
-		Supplier<Dog> supplier2 = Dog::new;
+		Supplier<User> supplier2 = User::new;
 		System.out.println(supplier2.get().age);
 	}
 }
