143d35ff8195de5e479f5e183def68701ae00c41
hoojo <hoojo@qq.com>
2019-07-11 11:03:05 +0800

    :sparkles: :bento: :recycle:  Changed stream example

--- src/io/github/hooj0/streams/StreamsExample.java
+++ src/io/github/hooj0/streams/StreamsExample.java
@@ -22,13 +22,37 @@ public class StreamsExample {
 	public static void main(String[] args) {
 
 		// list
-		Arrays.asList(1, 2, 3).stream().findFirst().ifPresent(System.out::println); // 1
+		Arrays.asList(1, 2, 3)
+				.stream()
+				.findFirst()
+				.ifPresent(System.out::println); // 1
 		
 		// stream
-		Stream.of(1, 2, 3).findFirst().ifPresent(System.out::println); // 1
+		Stream.of(1, 2, 3)
+			.findFirst()
+			.ifPresent(System.out::println); // 1
 		
 		// IntStream, LongStream and DoubleStream.
 		IntStream.range(1, 5).forEach(System.out::print); // 1234
 		
+		// 平均值
+		Arrays.stream(new int[] { 1, 3, 5 })
+				.map(n -> 2 * n + 1)
+				.average() // 平均值函数
+				.ifPresent(System.out::println); // 12347.0
+		
+		// 最大值
+		Stream.of("a1", "b2", "c3")
+			.map(s -> s.substring(1)) // 处理截取数据
+			.mapToInt(Integer::parseInt)	// 转换参数
+			.max()	// 最大值
+			.ifPresent(System.out::println); // 3
+		
+		// 转对象
+		IntStream.range(1, 4)
+				.mapToObj(n -> n + "1,") // 转对象
+				.forEach(System.out::print); // 11,21,31,
+		
+		//
 	}
 }
