77eb98f88bec79a183a5d72fa7de75353408952f
hoojo <hoojo@qq.com>
2019-07-14 16:03:14 +0800

    :sparkles: :bento: :tada:  Init add stream complex samples

---
+++ src/io/github/hooj0/streams/samples/StreamsComplexSample.java
@@ -0,0 +1,20 @@
+package io.github.hooj0.streams.samples;
+
+/**
+ * stream complex samples
+ * 
+ * @author hoojo
+ * @createDate 2019年8月15日 下午4:01:52
+ * @file StreamsComplexSample.java
+ * @package io.github.hooj0.streams.samples
+ * @project jdk8-features
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class StreamsComplexSample {
+
+	public static void main(String[] args) {
+
+	}
+}
