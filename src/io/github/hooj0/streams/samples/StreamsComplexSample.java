65d4965c573165a1a3f44051a96fc32b50c769c3
hoojo <hoojo@qq.com>
2019-07-15 16:43:27 +0800

    :sparkles: :bento: :recycle:  Changed stream complex samples

--- src/io/github/hooj0/streams/samples/StreamsComplexSample.java
+++ src/io/github/hooj0/streams/samples/StreamsComplexSample.java
@@ -1,5 +1,10 @@
 package io.github.hooj0.streams.samples;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
 /**
  * stream complex samples
  * 
@@ -14,7 +19,73 @@ package io.github.hooj0.streams.samples;
  */
 public class StreamsComplexSample {
 
+	// 将一个对象转换成多个其他对象
+	public static void testFlatMap() {
+		List<Foo> list = new ArrayList<>();
+		
+		// create foos
+		IntStream.range(1, 4).forEach(i -> list.add(new Foo("foo-" + i)));
+		
+		// create bars
+		list.forEach(f -> IntStream.range(1, 4).forEach(i -> f.bars.add(new Bar(f.name + "-bar" + i))));
+		
+		// views
+		list.stream().flatMap(f -> f.bars.stream()).forEach(b -> System.out.println(b.name));
+		
+		/*
+		  	foo-1-bar1
+			foo-1-bar2
+			foo-1-bar3
+			foo-2-bar1
+			foo-2-bar2
+			foo-2-bar3
+			foo-3-bar1
+			foo-3-bar2
+			foo-3-bar3
+		 */
+		
+		System.out.println("------------简化------------");
+		IntStream.range(1, 4)
+				.mapToObj(i -> new Foo("foo" + i))
+				.peek(f -> IntStream.range(1, 4).mapToObj(i -> new Bar(f.name + "-bar" + i)).forEach(f.bars::add))
+				.flatMap(f -> f.bars.stream())
+				.forEach(b -> System.out.println(b.name));
+	}
+	
+	
+	
 	public static void main(String[] args) {
+		testFlatMap();
+		
+		testFlatMapCheckNull();
+	}
+	
+	static class Outer {
+	    Nested nested;
+	}
 
+	class Nested {
+	    Inner inner;
+	}
+
+	class Inner {
+	    String foo;
+	}
+	
+	static class Foo {
+	    String name;
+	    List<Bar> bars = new ArrayList<>();
+
+	    Foo(String name) {
+	        this.name = name;
+	    }
+	}
+
+	static class Bar {
+	    String name;
+
+	    Bar(String name) {
+	        this.name = name;
+	    }
 	}
 }
